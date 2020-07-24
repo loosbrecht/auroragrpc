44375ea85b30afbd296830d2a30ba63d595a66d9
loosbrecht <loos.brecht@gmail.com>
2020-07-24 13:48:36 +0200

    initial code for vaadin

---
+++ auroragrpc-ui/src/main/java/com/loos/ui/MainView.java
@@ -0,0 +1,26 @@
+package com.loos.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+public class MainView extends VerticalLayout {
+
+
+    public MainView() {
+
+    }
+
+}
---
+++ electron/README.md
@@ -0,0 +1,3 @@
+https://github.com/cuba-labs/java-electron-tutorial
+
+https://vaadin.com/blog/cuba-studio-how-we-use-vaadin-for-our-web-development-tool
